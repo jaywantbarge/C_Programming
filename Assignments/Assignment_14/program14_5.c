2c6b3993d2397c03df42f4d7b9a49d09fc3b60a6
jaywantbarge <jaywantb891@gmail.com>
2025-11-03 20:43:07 +0530

    Assignment_14

---
+++ Assignments/Assignment_14/program14_5.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int Count(int iNo)
+{
+    int iCount = 0;
+    int iDigit = 0;
+    while(iNo > 0)
+    {
+        iDigit = iNo % 10;
+        if(iDigit < 6)
+        {
+            iCount++;
+        }
+        iNo = iNo / 10;
+    }
+    return iCount;
+}
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+    printf("Enter number\n");
+    scanf("%d",&iValue);
+    iRet = Count(iValue);
+    printf("%d", iRet);
+    return 0;
+}
